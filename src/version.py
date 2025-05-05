c3c47bdf4dd30dd2742d315b8e212f44676a643c
GitHub Action <action@github.com>
2025-05-05 01:49:48 +0000

    Auto-increment build number [skip ci]

--- src/version.py
+++ src/version.py
@@ -1,4 +1,4 @@
 """Version information"""
 # This file is automatically updated by the build process
-VERSION = "0.0.3.0"
-VERSION_TUPLE = (0, 0, 3, 0)
+VERSION = "0.0.5.0"
+VERSION_TUPLE = (0, 0, 5, 0)
