d11048e976b2bbc0322c019ace0c44bae8474485
GitHub Action <action@github.com>
2026-08-03 02:49:58 +0000

    Auto-increment build number [skip ci]

--- src/version.py
+++ src/version.py
@@ -1,4 +1,19 @@
 """Version information"""
 # This file is automatically updated by the build process
-VERSION = "0.0.13.0"
-VERSION_TUPLE = (0, 0, 13, 0)
+# DO NOT EDIT MANUALLY
+
+VERSION = "0.0.14.0"
+VERSION_TUPLE = (0, 0, 14, 0)
+BUILD_TIME = "2026-08-03T02:49:58.752947Z"
+
+# Version components
+MAJOR = 0
+MINOR = 0
+BUILD = 14
+REVISION = 0
+
+# Git information
+GIT_COMMIT = "27708f2d"
+GIT_COMMIT_FULL = "27708f2df16c0f59d656bb9470ab2e94c6189c30"
+GIT_COMMIT_COUNT = 57
+GIT_DIRTY = False
